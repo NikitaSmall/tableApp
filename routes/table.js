b00055f9d1db931759dafc4260809e8e742523fd
nikitasmall <nikitasosnov@yahoo.com>
2018-01-13 00:21:32 +0200

    add table page; handle it

--- routes/table.js
+++ routes/table.js
@@ -1,9 +1,19 @@
 var express = require('express');
+
+var path = require('path');
+var fs = require('fs');
+
+var csvParse = require('csv-parse/lib/sync');
 var router = express.Router();
 
 /* GET table page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var file = fs.readFileSync(path.join(__dirname, '..', 'data', 'example_data.csv'));
+  var data = csvParse(file, { columns: true });
+
+  res.render('table', { title: 'Table',
+                        data: data,
+                        admin: (req.query.context == "admin") });
 });
 
 module.exports = router;
